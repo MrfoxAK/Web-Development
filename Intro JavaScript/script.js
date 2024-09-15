9e4103025b6985f99fa43254157c836569d096ae
Akash Das <akd270302@gmail.com>
2024-09-15 16:14:09 +0530

    intro of Js

---
+++ Intro JavaScript/script.js
@@ -0,0 +1,33 @@
+alert("Hello World");
+
+console.log("Console is running...")
+
+
+
+var a = prompt("Enter Your Number: ")
+
+var isTrue = confirm("Are you sure you want to leave this page and blast your computer ")
+
+if(isTrue){
+    console.log("Computer is blasting")
+}
+
+else{
+    console.log("Computer is not blasting")
+
+}
+console.log("Your number is " + a)
+
+
+document.title = "Hey I am good"
+
+
+
+
+
+
+
+
+
+
+
