837eb0f169b9be22345774e4d2c972bd8048e127
Akash Das <akd270302@gmail.com>
2024-10-02 19:48:37 +0530

    for loops

---
+++ JavaScript Full Course/loops/script.js
@@ -0,0 +1,33 @@
+// for loop
+
+// for(let i = 0; i<35; i++){
+//      console.log(i);
+
+// }
+
+
+const object = {
+     "Harray": true,
+     "Shubh": false,
+     "Lev": 102,
+     "Rohan": undefined
+}
+
+// for-in loop
+
+// for(let a in object){
+//      console.log(`Marks of ${a} is ${object[a]}`);
+// }
+// for (const key in object) {
+//      if (Object.prototype.hasOwnProperty.call(object, key)) {
+//           const element = object[key];
+//           console.log(element);
+//      }
+// }
+
+
+// For of loop
+
+for (let b of "Harry") {
+     console.log(b)
+}
