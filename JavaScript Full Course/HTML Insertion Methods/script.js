f402fe0e15a2d9245e02ee99ffb53b74b60e6451
Akash Das <akd270302@gmail.com>
2024-10-03 21:13:53 +0530

    HTML Insertion Methods

---
+++ JavaScript Full Course/HTML Insertion Methods/script.js
@@ -0,0 +1,39 @@
+
+
+let a = document.getElementsByTagName('div')[0]
+
+
+
+// a.innerHTML = a.innerHTML + '<h1>Hey There</h1>';
+
+
+
+
+let s = document.createElement('span');
+s.innerHTML = "Hy There"
+// a.append(s)
+// a.prepend(s)
+// a.before(s)
+// a.after(s)
+a.replaceWith('div')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
