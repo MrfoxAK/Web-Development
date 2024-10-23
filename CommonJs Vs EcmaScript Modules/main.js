bd4fde2e541aabbbac893c79550796d63390486f
Akash Das <akd270302@gmail.com>
2024-10-23 11:09:39 +0530

    commonjs vs EcmaScript

---
+++ CommonJs Vs EcmaScript Modules/main.js
@@ -0,0 +1,11 @@
+// import {a, b} from "./mymodule.js"
+
+// console.log(a);
+// console.log(b);
+
+import o from "./mymodule.js"
+console.log(o);
+
+// const a = require("./mymodule2.js");
+
+// console.log(a);
---
+++ CommonJs Vs EcmaScript Modules/mymodule.js
@@ -0,0 +1,7 @@
+export const a = 1; //named export
+export const b = 2;
+let obj;
+export default obj = {
+     x : 5,
+     y : 10
+}
