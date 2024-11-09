d449ab84525d39d643675abc480235037e547bf9
Akash Das <akd270302@gmail.com>
2024-11-09 12:26:48 +0530

    modification on mini project

--- BackEnd By Sheryians Coding School/node_js/Ejs_dynamic_routing/index.js
+++ BackEnd By Sheryians Coding School/node_js/Ejs_dynamic_routing/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
 
 
 app.use(express.json());
@@ -10,11 +11,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
-     res.render("index");
+     fs.readdir(`./files`, function(err,files){
+          res.render("index", {files: files});
+     });
 });
 
-app.get("/profile/:username", function(req, res){
-     res.send(req.params.username);
+app.get("/files/:filename", function(req, res){
+     fs.readFile(`./files/${req.params.filename}`, "utf-8", function(err,filedata){
+          res.render("show", {filename: req.params.filename , filedata: filedata});
+     });
+});
+
+app.post("/create", function(req, res){
+     fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){
+          res.redirect("/");
+     });
 });
 
 app.listen(3000, function(){
