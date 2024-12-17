68d45d73aeb548709b8e8e7a8333c4573a20c56e
Akash Das <akd270302@gmail.com>
2024-12-17 22:00:56 +0530

    completed the url shortener backend project

--- Projects/Custom URL Shortener/index.js
+++ Projects/Custom URL Shortener/index.js
@@ -2,16 +2,32 @@ const express = require('express');
 const { connectToMongoDB } = require('./connect');
 const urlRoute = require('./routes/url');
 const URL = require('./models/url');
+const path = require('path');
+const staticRoute = require('./routes/staticRouter');
 
 const app = express();
 const PORT = 8001;
 
 connectToMongoDB('mongodb://localhost:27017/short-url').then(() => console.log('MongoDB connection established'));
 
-app.use(express.json());
-app.use("/url", urlRoute);
+app.set('view engine', 'ejs');
+app.set("views", path.resolve("./views"));
 
-app.get('/:shortID', async (req, res) => {
+app.use(express.json());
+app.use(express.urlencoded({ extended:false }));
+
+app.get("/test", async (req, res) => {
+     const allurls = await URL.find({});
+     return res.render('home',{
+          urls: allurls,
+     });
+});
+
+
+app.use("/url", urlRoute);
+app.use("/", staticRoute);
+
+app.get('/url/:shortID', async (req, res) => {
      const shortID = req.params.shortID;
      console.log(shortID);
      const entry = await URL.findOneAndUpdate(
@@ -25,7 +41,7 @@ app.get('/:shortID', async (req, res) => {
           },
      });
      console.log(entry);
-     
+
      res.redirect(entry.redirectURL);
 });
 
