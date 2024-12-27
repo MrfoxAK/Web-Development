394babbf53781dc3370c9834a836a6ede6389d01
Akash Das <akd270302@gmail.com>
2024-12-27 13:05:51 +0530

    `Added express-session and connect-flash middleware, updated routes and views to use them, and made minor changes to code organization and formatting.`

--- Projects/Bag Shop/app.js
+++ Projects/Bag Shop/app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
 const path = require('path');
+const expressSession = require('express-session');
+const flash = require('connect-flash');
 const ownersRouter = require('./routes/ownersRouter');
 const usersRouter = require('./routes/usersRouter');
 const productsRouter = require('./routes/productsRouter');
 const indexRouter = require('./routes/index');
-
 
 require('dotenv').config();
 // Example debug usage
@@ -19,6 +20,14 @@ const db = require('./config/mongoose-connection');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+     expressSession({
+          secret: process.env.EXPRESS_SESSION_SECRET,
+          resave: false,
+          saveUninitialized: false
+     })
+);
+app.use(flash());
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
--- Projects/Bag Shop/routes/usersRouter.js
+++ Projects/Bag Shop/routes/usersRouter.js
@@ -1,24 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const userModel = require('../models/user-model');
+const {
+     registerUser,
+     loginUser,
+     logout,
+} = require('../controllers/authController');
+
 
 router.get('/', function (req, res) {
      res.send("hey it's user");
 });
 
-router.post('/register', async function (req, res) {
-     try {
-          let { email, password, fullname } = req.body;
-          let user = await userModel.create({
-               email,
-               password,
-               fullname,
-          });
-          res.status(201).json(user);
-     }
-     catch (err) {
-          res.send(err.message);
-     }
-});
+router.post('/register', registerUser);
+
+router.post('/login', loginUser);
+
+router.post('/logout', logout);
 
 module.exports = router;
