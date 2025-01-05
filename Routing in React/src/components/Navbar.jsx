158f555ee4744dee16012d2503d2173015ae143a
Akash Das <akd270302@gmail.com>
2025-01-06 00:06:26 +0530

    `Added cart functionality and user authentication to Bag Shop application`

--- Routing in React/src/components/Navbar.jsx
+++ Routing in React/src/components/Navbar.jsx
@@ -5,9 +5,9 @@ const Navbar = () => {
      return (
           <div>
                <nav>
-                    <NavLink className={(e)=> {return e.isActive?"red": "" }} to="/"><li>Home</li></NavLink>
-                    <NavLink className={(e)=> {return e.isActive?"red": "" }} to="/about"><li>About</li></NavLink>
-                    <NavLink className={(e)=> {return e.isActive?"red": "" }} to="/login"><li>Login</li></NavLink>
+                    <NavLink className={(e)=> {return e.isActive?"blue": "" }} to="/"><li>Home</li></NavLink>
+                    <NavLink className={(e)=> {return e.isActive?"blue": "" }} to="/about"><li>About</li></NavLink>
+                    <NavLink className={(e)=> {return e.isActive?"blue": "" }} to="/login"><li>Login</li></NavLink>
                </nav>
           </div>
      )
