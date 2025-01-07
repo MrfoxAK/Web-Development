ff3c0bddb70237c39bedca67160731ced809ccff
Akash Das <akd270302@gmail.com>
2025-01-07 18:10:31 +0530

    `Added show finished toggle and disabled save button when todo is too short`

--- Projects/TodoList/src/App.jsx
+++ Projects/TodoList/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   const [todos, setTodos] = useState([]) // Initialize as an empty array
   const [isDialogOpen, setIsDialogOpen] = useState(false)
   const [todoIdToDelete, setTodoIdToDelete] = useState(null)
+  const [showFinished, setshowFinished] = useState(true)
 
   // Load todos from localStorage on component mount
   useEffect(() => {
@@ -23,6 +24,11 @@ function App() {
       localStorage.setItem("todos", JSON.stringify(todos))
     }
   }, [todos]) // This will run every time the todos state changes
+
+
+  const toggleFinished = (e) => {
+    setshowFinished(!showFinished)
+  }
 
   const handleEdit = (e, id) => {
     let t = todos.filter(i => i.id === id)
@@ -70,17 +76,18 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className="container mx-auto bg-violet-100 rounded-xl p-5 my-10 min-h-[80vh]">
+      <div className="container mx-auto bg-violet-100 rounded-xl p-5 my-10 min-h-[80vh] w-3/4 sm:w-1/2 text-sm md:text-lg max-w-[500px]:text-xs">
         <div className="addTodo my-5">
           <h2 className='text-lg font-bold'>Add a Todo</h2>
           <input onChange={handleChange} value={todo} type="text" className='w-1/2' />
-          <button onClick={handleAdd} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-6'>Save</button>
+          <button onClick={handleAdd} disabled={todo.length<=3} className='bg-violet-800 hover:bg-violet-950 disabled:bg-violet-500 p-2 py-1 text-sm font-bold text-white rounded-md mx-6'>Save</button>
         </div>
+        <input type="checkbox" onChange={toggleFinished} checked={showFinished} />Show Finished
         <h2 className='text-lg font-bold'>Your Todos</h2>
         <div className="todos">
           {todos.length === 0 && <div className='m-5'>No Todos to Display</div>}
           {todos.map(item => (
-            <div key={item.id} className="todo flex w-1/4 my-3 justify-between">
+            (showFinished || !item.isCompleted) && <div key={item.id} className="todo flex my-3 justify-between w-full">
               <div className='flex gap-5'>
                 <input
                   name={item.id}
@@ -90,7 +97,7 @@ function App() {
                 />
                 <div className={item.isCompleted ? "line-through" : ""}>{item.todo}</div>
               </div>
-              <div className="buttons">
+              <div className="buttons flex h-full">
                 <button onClick={(e) => { handleEdit(e, item.id) }} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1'>Edit</button>
                 <button onClick={() => handleConfirmDelete(item.id)} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1'>Delete</button>
               </div>
