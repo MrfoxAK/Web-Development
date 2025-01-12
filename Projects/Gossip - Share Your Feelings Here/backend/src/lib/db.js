f41958fe771143aa150171f7a8eab5cf410d2fc1
Akash Das <akd270302@gmail.com>
2025-01-12 23:47:01 +0530

    completed

--- Projects/Gossip - Share Your Feelings Here/backend/src/lib/db.js
+++ Projects/Gossip - Share Your Feelings Here/backend/src/lib/db.js
@@ -1,11 +1,10 @@
 import mongoose from "mongoose";
 
-export const connectDB = async ()=>{
-     try{
-          const conn = await mongoose.connect(process.env.MONGODB_URL);
-          console.log(`Mongoose Connected: ${conn}`);
-          
-     }catch(error){
-          console.log("Mongoose Error", error);
-     }
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log("MongoDB connection error:", error);
+  }
 };
